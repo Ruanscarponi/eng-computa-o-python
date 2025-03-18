483452c5a378453c2f37ff987a1ea0df92e869b2
primeito teste <ranbarbosascarponi@gmail.com>
2025-03-17 22:48:47 -0300

    meu primeiro rograma em pythonprin

---
+++ primeiro exercicio.py
@@ -0,0 +1,20 @@
+print("Ruan scarponi\n")
+
+# EXERCICIO 1
+a = 3
+b = 2
+print(2*a*3*b)
+print()
+# EXERCICIO 3
+print()
+num1 = 2
+num2 = 3
+num3 = 4
+soma = num1+num2+num3
+print (soma)
+num1 = float(input("Numero 1 ="))
+num2 = float(input("Numero 2 ="))
+num3 = float(input("Numero 3 ="))
+soma = num1+num2+num3
+print(f"soma = {soma:.2f}")
+
--- primerio programa.py
+++ primerio programa.py
@@ -1,1 +1,7 @@
-print("hello world")
+
+print("Ruan scarponi\n")
+
+# EXERCICIO 1
+a = 3
+b = 2
+print(2*a*3*b)
