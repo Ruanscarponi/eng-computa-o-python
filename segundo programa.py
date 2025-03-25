2d05847f522a3ed6ee427953985654177410f407
labsfiap <ruanbarbosascarponi@gmail.com>
2025-03-24 22:47:39 -0300

    exemplos aula 2

---
+++ segundo programa.py
@@ -0,0 +1,13 @@
+print("comparaçãon\n")
+nota = 8
+media = 7
+aprovado = nota >= media
+print(aprovado)
+
+nome_completo = input("nome completo:")
+idade = int(input("idade: "))
+altura = float(input("altura:"))
+
+print(nome_completo)
+print(idade)
+print(altura)
